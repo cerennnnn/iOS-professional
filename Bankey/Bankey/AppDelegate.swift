f391fbbbf4ce9bfa14b95a97e1e49b9d75d703c6
cerennnnn <cerengunnes@gmail.com>
2023-07-24 11:12:59 +0300

    Add first onboarding screen.

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
 //        window?.rootViewController = LoginViewController()
-        window?.rootViewController = OnboardingContainerViewController()
+//        window?.rootViewController = OnboardingContainerViewController()
+        window?.rootViewController = OnboardingViewController()
         
         return true
     }
---
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,57 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Ceren Güneş on 24.07.2023.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        // Image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        // Label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 0
+        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
