1cb9556a52f7d129fcc5e68e2dd6b05c2357eb07
cerennnnn <cerengunnes@gmail.com>
2023-08-06 00:46:30 +0300

    Deleted old Password folder and added a new one.

--- Password/Password/AppDelegate.swift
+++ Password/Password/AppDelegate.swift
@@ -2,23 +2,25 @@
 //  AppDelegate.swift
 //  Password
 //
-//  Created by Ceren Güneş on 3.08.2023.
+//  Created by jrasmusson on 2022-01-23.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
         window?.rootViewController = ViewController()
         
-        return true 
+        return true
     }
-    
+
 }
 
---
+++ Password/Password/PasswordStatusView/PasswordCriteriaView.swift
@@ -0,0 +1,96 @@
+//
+//  PasswordCriteriaView.swift
+//  Password
+//
+//  Created by Ceren Güneş on 4.08.2023.
+//
+
+import UIKit
+
+class PasswordCriteriaView: UIView {
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    let checkmarkImage = UIImage(systemName: "checkmark.circle")!.withTintColor(.systemGreen, renderingMode: .alwaysOriginal)
+    let xmarkImage = UIImage(systemName: "xmark.circle")!.withTintColor(.systemRed, renderingMode: .alwaysOriginal)
+    let circleImage = UIImage(systemName: "circle")!.withTintColor(.tertiaryLabel, renderingMode: .alwaysOriginal)
+    
+    var isCriteriaMet: Bool = false {
+        didSet {
+            if isCriteriaMet {
+                imageView.image = checkmarkImage
+            } else {
+                imageView.image = xmarkImage
+            }
+        }
+    }
+    
+    func reset() {
+        isCriteriaMet = false
+        imageView.image = circleImage
+    }
+    
+    init(text: String) {
+        super.init(frame: .zero)
+        
+        label.text = text
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 40)
+    }
+}
+
+extension PasswordCriteriaView {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "circle")!.withTintColor(.tertiaryLabel, renderingMode: .alwaysOriginal)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.textColor = .secondaryLabel
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        addSubview(stackView)
+        
+        // Stack
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        // Image
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        ])
+        
+        // CHCR
+        imageView.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: .horizontal)
+        label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+    }
+}
+
+//control + command + back arrow -> 1 sayfa geriye gider
+//control + command + forward arrow -> 1 sayfa ileriye gider
